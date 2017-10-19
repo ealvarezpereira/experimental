a66d33a9e2edd2e2e977bd2a6f94251995fcfd6b
quique <quique@quique-VirtualBox>
2017-10-19 09:41:12 +0200

    choripan_pantumaka

---
+++ src/choripan_pantumaka/Choripan_pantumaka.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package choripan_pantumaka;
+
+/**
+ *
+ * @author quique
+ */
+public class Choripan_pantumaka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Tu sabes si la hermeneutica telurica incaica transtrueca de la peripatetica neoterica de la filosofia aristotelica, se ve influenciada por inocuidad factica de los dialogos socraticos no dogmaticos? ");
+    }
+    
+}
